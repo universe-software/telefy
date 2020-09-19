5453bb2aa1735f6a044451cccd6b8abc994ec54d
Hyperdraw <plexi.eater@gmail.com>
2020-09-19 10:23:32 -0500

    Fix update-version.sh to set valid version

--- update-version.sh
+++ update-version.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i -r "s/version=\".*\"/version=\"0.$CIRCLE_BUILD_NUM\"/" config.xml
-sed -i -r "s/\"version\": \".*\"/\"version\": \"0.$CIRCLE_BUILD_NUM\"/" package.json
+sed -i -r "s/version=\".*\"/version=\"0.1.$CIRCLE_BUILD_NUM\"/" config.xml
+sed -i -r "s/\"version\": \".*\"/\"version\": \"0.1.$CIRCLE_BUILD_NUM\"/" package.json
