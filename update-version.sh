eace528ea7a40484973db5df3e444bcdba0b0d45
Hyperdraw <plexi.eater@gmail.com>
2020-09-19 10:12:39 -0500

    Fix CIRCLECI_BUILD_NUM to CIRCLE_NUILD_NUM

--- update-version.sh
+++ update-version.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i -r "s/version=\".*\"/version=\"0.$CIRCLECI_BUILD_NUM\"/" config.xml
-sed -i -r "s/\"version\": \".*\"/\"version\": \"0.$CIRCLECI_BUILD_NUM\"/" package.json
+sed -i -r "s/version=\".*\"/version=\"0.$CIRCLE_BUILD_NUM\"/" config.xml
+sed -i -r "s/\"version\": \".*\"/\"version\": \"0.$CIRCLE_BUILD_NUM\"/" package.json
